f79e5e5cf70c9715e4e78b0ef63b7b70344b2091
Camtelle <camtelle@Camtelles-Mac-mini.local>
2026-03-22 13:48:33 -0600

    Added checkout

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getProductById, getProducts } from "../data/products";
+import { useCart } from "../context/CartContext";
 
 export default function ProductDetails() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const navigate = useNavigate();
+  const { addToCart, cartItems } = useCart();
 
   useEffect(() => {
     const foundProduct = getProductById(id);
@@ -20,6 +22,9 @@ export default function ProductDetails() {
   if (!product) {
     return <h1>Loading...</h1>;
   }
+
+  const productInCart = cartItems.find((item) => item.id === product.id);
+  const productQuantityLabel = productInCart ? `${productInCart.quantity}` : "";
   return (
     <div className="page">
       <div className="container">
@@ -31,7 +36,12 @@ export default function ProductDetails() {
             <h1 className="product-detail-name">{product.name}</h1>
             <p className="product-detail-price">{product.price}</p>
             <p lassName="product-detail-description">{product.description}</p>
-            <button className="btn btn-primary">Add to Cart</button>
+            <button
+              className="btn btn-primary"
+              onClick={() => addToCart(product.id)}
+            >
+              Add to Cart {productQuantityLabel}
+            </button>
           </div>
         </div>
       </div>
